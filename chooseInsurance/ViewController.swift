d22281683c096d312edf4c3dac016c8dfa7b6ec1
Louis <louis@Louisde-MacBook-Air.local>
2017-08-10 21:48:18 +0800

    Initial Commit

---
+++ chooseInsurance/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  chooseInsurance
+//
+//  Created by Louis on 2017/8/10.
+//  Copyright © 2017年 Louis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
